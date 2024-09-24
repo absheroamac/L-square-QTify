699ee1dd9c46f187a2c2d1de6c07477c387a8e1e
Absher K <absheroamac@gmail.com>
2024-09-24 21:30:36 +0530

    Added new section

--- qtify/src/components/CardItem.jsx
+++ qtify/src/components/CardItem.jsx
@@ -1,28 +1,36 @@
-import { Card, CardContent, CardMedia, Chip, Typography , Box} from '@mui/material';
-import styles from './CardItem.module.css';
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Chip,
+  Typography,
+  Box,
+} from "@mui/material";
+import styles from "./CardItem.module.css";
 
 function CardItem({ card }) {
+  console.log(card.img);
+
   return (
-    <Box>
-    <Card sx={{borderRadius:'10px', width:'100%'}}>
-      <CardMedia 
-      component='img'
-      image={card.img}
-      height="170"
-      width={'100%'}/>
-  
-      <Chip sx={{ margin: '8px 8px', backgroundColor:'black', color:'white'}} label={card.follows}/>
-  
-    </Card>
+    <div sx={{ width: "100%" }}>
+      <Card sx={{ borderRadius: "10px", width: "100%" }}>
+        <CardMedia
+          component="img"
+          image={card.image}
+          height="170"
+          width={"100%"}
+        />
 
-    <Typography sx={{color:
-      'white', mt:'6px', fontSize:'14px'
-    }}>
-      {card.title}
-    </Typography>
-    </Box>
+        <Chip
+          sx={{ margin: "8px 8px", backgroundColor: "black", color: "white" }}
+          label={card.follows + "Follows"}
+        />
+      </Card>
 
-
+      <Typography sx={{ color: "white", mt: "6px", fontSize: "14px" }}>
+        {card.title}
+      </Typography>
+    </div>
   );
 }
 
--- qtify/src/components/Section.jsx
+++ qtify/src/components/Section.jsx
@@ -1,38 +1,35 @@
 import { Button, Typography } from "@mui/material";
-import { Box, Grid } from "@mui/system";
+import { Box, Grid, padding } from "@mui/system";
 import CardItem from "./CardItem";
+import Styles from "./Section.module.css";
 
-function Section({data}){
-
-    return(
-        <Box>
-            <Box>
-                <Typography>
-                    Title
-                </Typography>
-
-                <Button>
-                    Title
-                </Button>
-            </Box>
-            <Box>
-                <Grid container spacing={2}>
-                    {
-                    data.map((element)=>{
-                        return(<Grid item md={2} key={element.id}>
-                            <CardItem card={element}/>
-                        </Grid>)
-                    })
-                }
-                    
-                </Grid>
-            </Box>
-        </Box>
-    )
-
-
-
-
+function Section({ data }) {
+  return (
+    <Box
+      sx={{
+        padding: "2rem",
+        display: "flex",
+        flexDirection: "column",
+        gap: "0.5rem",
+      }}
+    >
+      <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Typography sx={{ color: "white", fontSize: "20px" }}>Title</Typography>
+        <Button sx={{ color: "#34C94B", textTransform: "none" }}>
+          Collapse
+        </Button>
+      </Box>
+      <Grid className={Styles.grid}>
+        {data.map((element) => {
+          return (
+            <Grid item md={2} key={element.id}>
+              <CardItem card={element} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
+  );
 }
 
 export default Section;
